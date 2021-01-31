178c7350dc77d7da64bb322266f5840c43fbbcd7
TheMarpe <martin.peterlin7@gmail.com>
2021-01-31 15:54:24 +0100

    Fixed indentation

--- src/Runner.Listener/MessageListener.cs
+++ src/Runner.Listener/MessageListener.cs
@@ -86,7 +86,7 @@ namespace GitHub.Runner.Listener
 
                     _term.WriteLine();
                     _term.WriteSuccessMessage("Connected to GitHub");
-		    _term.WriteSuccessMessage("This is a forked version!");
+		            _term.WriteSuccessMessage("This is a forked version!");
                     _term.WriteLine();
 
                     _session = await _runnerServer.CreateAgentSessionAsync(
